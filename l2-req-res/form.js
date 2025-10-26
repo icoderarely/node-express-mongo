d974cc3851ace106a1b711ea0d9dd4cc74bb17d4
semicolon guy <itworksnavneet@gmail.com>
2025-10-26 20:17:36 +0530

    redirection

--- l2-req-res/form.js
+++ l2-req-res/form.js
@@ -1,12 +1,21 @@
 const http = require("http");
+const fs = require("fs");
 
 function requestHandler(req, res) {
   console.log(req.url, req.method, req.headers);
+  // routing
+
+  if (req.url.toLowerCase() === "/submit" && req.method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
 
-  // routing
   if (req.url === "/") {
     res.write("<body><h1>Welcome to the Home Page!</h1></body></html>");
     return res.end();
@@ -21,7 +30,8 @@ function requestHandler(req, res) {
     res.write(
       "<input type='password' name='password' placeholder='Password'/>",
     );
-    res.write("<button type='submit'>Submit</button></html>");
+    // res.write("<button type='submit'>Submit</button></html>");
+    res.write("<input type='submit' value='Submit'/>");
     return res.end();
   }
 
